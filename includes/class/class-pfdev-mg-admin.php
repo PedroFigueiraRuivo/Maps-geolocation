0e220ac156c4e4a13793db11235829a1969d19c6
Pedro Figueira <pe.fogueyra@gmail.com>
2022-08-22 16:36:34 -0300

    finalização do admin e inicialização do frontend

--- includes/class/class-pfdev-mg-admin.php
+++ includes/class/class-pfdev-mg-admin.php
@@ -17,52 +17,50 @@
 
       add_action( 'admin_menu', [ $this, 'pfDev__mg_addPluginMenuPage' ] );
       add_action( 'admin_init', [ $this, 'pfr__mg_pageInit' ] );
-      // add_action( 'admin_footer_text', [ $this, 'pfr__wsa__pageFooter' ] );
       add_filter( 'plugin_action_links_' . $this->plugin_basename, [ $this, 'pfDev_add_settings_link' ] );
     }
 
     public function pfDev__mg_addPluginMenuPage(){
       if ( empty ( $GLOBALS['admin_page_hooks']['pfdev'] ) ){
         add_menu_page(
-            __( 'pfDev: Settings' ),
-            'pfDev',
-            'manage_options',
-            'pfdev',
-            function() { echo "<h1>Page pfDev</h1>"; }
+          __( 'pfDev: Settings' ),
+          'pfDev',
+          'manage_options',
+          'pfdev',
+          function() { echo "<h1>Page pfDev</h1>"; }
         );
       }
 
       add_submenu_page(
-          'pfdev',
-          $this->plugin_name,
-          $this->plugin_name,
-          'edit_theme_options',
-          $this->plugin_slug,
-          [ $this, 'pfDev__mg_pageOptions' ]
+        'pfdev',
+        $this->plugin_name,
+        $this->plugin_name,
+        'edit_theme_options',
+        $this->plugin_slug,
+        [ $this, 'pfDev__mg_pageOptions' ]
       );
     }
 
     public function pfDev__mg_pageOptions(){
-      // print_r ($this->plugin_configurationFields);
       ?>
-          <div class="wrap">
-              <h1><?php echo $this->plugin_name; ?></h1>
-              <form method="post" action="options.php">
-                  <?php
-                  settings_fields( $this->plugin_slug_db . '_options' );
-                  do_settings_sections( $this->plugin_slug . '-admin' );
-                  submit_button();
-                  ?>
-              </form>
-          </div>
+        <div class="wrap">
+          <h1><?php echo $this->plugin_name; ?></h1>
+          <form method="post" action="options.php">
+            <?php
+              settings_fields( $this->plugin_slug_db . '_options' );
+              do_settings_sections( $this->plugin_slug . '-admin' );
+              submit_button();
+            ?>
+          </form>
+        </div>
       <?php
     }
 
     public function pfr__mg_pageInit(){
       register_setting(
-          $this->plugin_slug_db . '_options',
-          $this->plugin_slug_db,
-          [ $this, 'sanitize' ]
+        $this->plugin_slug_db . '_options',
+        $this->plugin_slug_db,
+        [ $this, 'sanitize' ]
       );
 
       $arrConfig = $this->plugin_configurationFields;
@@ -79,10 +77,13 @@
           add_settings_field(
             $arrConfig[$II]['fields'][$JJ]['label_under_slug'],
             __( $arrConfig[$II]['fields'][$JJ]['label_text'], $this->plugin_slug),
-            [ $this, 'pfDev_callback'],
+            [ $this, $arrConfig[$II]['fields'][$JJ]['callback']],
             $this->plugin_slug . '-admin',
             'settings_section_id_' . ($II + 1),
             [ //args
+              'field'         => $arrConfig[$II]['fields'][$JJ]['field'],
+              'style'         => $arrConfig[$II]['fields'][$JJ]['style'],
+              'class'         => $arrConfig[$II]['fields'][$JJ]['class'],
               'base'          => $arrConfig[$II]['fields'][$JJ]['label_under_slug'],
               'description'   => $arrConfig[$II]['fields'][$JJ]['description_field'],
               'typeField'     => $arrConfig[$II]['fields'][$JJ]['type_field']
@@ -92,19 +93,41 @@
       }
     }
 
-    public function pfDev_callback($args) {
+    public function pfDev_callback_input($args) {
       $value = isset($this->options[$args['base']]) ? (
         esc_attr($this->options[$args['base']])
       ) : ('');
 
       ?>
-        <input 
+        <<?php echo $args['field']?>
+          style="<?php echo $args['style']?>"
           type="<?php echo $args['typeField'] ?>" 
           id="<?php echo $args['base']?>" 
-          name="<?php echo $this->plugin_slug_db . '[' . $args['base'] . ']' ;?>"  
+          name="<?php echo $this->plugin_slug_db . '[' . $args['base'] . ']' ;?>"
           value="<?php echo $value; ?>" 
-          class="regular-text"
-        ><br>
+          class="<?php echo $args['class']?>"
+        >
+        <br>
+        <p class="description">
+          <?php echo __( $args['description'], $this->plugin_slug ); ?>
+        </p>
+      <?php
+    }
+
+    public function pfDev_callback_textarea($args) {
+      $value = isset($this->options[$args['base']]) ? (
+        esc_attr($this->options[$args['base']])
+      ) : ('');
+
+      ?>
+        <<?php echo $args['field']?>
+          style="<?php echo $args['style']?>"
+          type="<?php echo $args['typeField'] ?>" 
+          id="<?php echo $args['base']?>" 
+          name="<?php echo $this->plugin_slug_db . '[' . $args['base'] . ']' ;?>"
+          class="<?php echo $args['class']?>"
+        ><?php echo ($value . '</' . $args['field'] . '>');?>
+        <br>
         <p class="description">
           <?php echo __( $args['description'], $this->plugin_slug ); ?>
         </p>
@@ -118,9 +141,14 @@
 
       for ($II = 0; $II < count($arrConfig); $II++) {
         for ($JJ = 0; $JJ < count($arrConfig[$II]['fields']); $JJ++) {
-          if( isset( $arrConfig[$II]['fields'][$JJ]['label_under_slug']) ){
-              $valueUnderLabel = $arrConfig[$II]['fields'][$JJ]['label_under_slug'];
-              $new_input[ $valueUnderLabel ] = sanitize_text_field( $input[ $valueUnderLabel ] );
+          if(isset($arrConfig[$II]['fields'][$JJ]['label_under_slug'])){
+
+            $valueUnderLabel = $arrConfig[$II]['fields'][$JJ]['label_under_slug'];
+            if ($arrConfig[$II]['fields'][$JJ]['field'] === 'textarea') {
+              $new_input[ $valueUnderLabel ] = sanitize_textarea_field($input[$valueUnderLabel]);
+            } else {
+              $new_input[ $valueUnderLabel ] = sanitize_text_field($input[$valueUnderLabel]);
+            }
           }
         }
       }
